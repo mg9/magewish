1a5a0fa5ba67433c57e8b3389fc01f01eb1f1699
Müge Kural <muge.kural@eteration.com>
2017-07-07 18:17:38 +0300

    asdas

--- wish/polly-wish/src/main/js/webpack.config.js
+++ wish/polly-wish/src/main/js/webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 var path = require('path');
 
 //var BUILD_DIR = path.resolve(__dirname, 'src/main/resources/static/built');
-var BUILD_DIR = path.resolve(__dirname, 'dist');
+var BUILD_DIR = path.resolve(__dirname, 'public');
 
 var APP_DIR = path.resolve(__dirname, 'src');
 
